843613e8d9f7fce70c69d8b8c0b4d0479efa9d23
陈总 <chenzhong100@outlook.com>
2024-06-14 16:58:30 +0800

    kotlin rxjava2 retrofit2

--- app/src/main/java/com/zchan/splash/view/ShopFragment.kt
+++ app/src/main/java/com/zchan/splash/view/ShopFragment.kt
@@ -2,11 +2,13 @@ package com.zchan.splash.view
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.example.bilibili.R
+import com.jakewharton.rxbinding2.view.RxView
 import com.zchan.splash.model.HttpUtil
 import com.zchan.splash.model.api.WangAndroidApi
 import com.zchan.splash.model.bean.ProjectBean
@@ -15,6 +17,7 @@ import com.zchan.splash.view.base.BaseFragment
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 
@@ -29,45 +32,50 @@ class ShopFragment : BaseFragment(), View.OnClickListener {
         view.findViewById<Button>(R.id.action).setOnClickListener(this@ShopFragment)
     }
 
+
+    @SuppressLint("CheckResult")
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.action -> {
-                lateinit var processDialog: AlertDialog
-                api = HttpUtil.getRetrofit().create(WangAndroidApi::class.java)
-                api!!.getProject()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(object : Observer<ProjectBean> {
-                        override fun onSubscribe(d: Disposable) {
-                            processDialog = AlertDialog.Builder(context).apply {
-                                setTitle("提示")
-                                setMessage("正在加载中...")
-                                setCancelable(false)
-                                setPositiveButton("取消") { dialog, which ->
-                                    d.dispose()
-                                    dialog.dismiss()
-                                }
-                            }.create()
-                            processDialog.show()
-                            Log.d("ShopFragment", "onSubscribe")
-                        }
+                lateinit var processDialog: ProgressDialog
+                RxView.clicks(v)
+                    .throttleFirst(2, java.util.concurrent.TimeUnit.SECONDS)
+                    .subscribeOn(AndroidSchedulers.mainThread())
+                    .subscribe(object : Consumer<Any> {
+                        override fun accept(t: Any) {
+                            api = HttpUtil.getRetrofit().create(WangAndroidApi::class.java)
+                            api!!.getProject()
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(object : Observer<ProjectBean> {
+                                    override fun onSubscribe(d: Disposable) {
+                                        processDialog = ProgressDialog.show(
+                                            context,
+                                            "加载中",
+                                            "请稍等",
+                                            false,
+                                            false
+                                        )
+                                    }
 
-                        override fun onNext(t: ProjectBean) {
-                            Toast.makeText(context, t.toString(), Toast.LENGTH_SHORT).show()
-                            Log.d("ShopFragment", "onNext")
-                        }
+                                    override fun onNext(t: ProjectBean) {
+                                        Toast.makeText(context, t.toString(), Toast.LENGTH_SHORT)
+                                            .show()
+                                    }
 
-                        override fun onError(e: Throwable) {
-                            processDialog.dismiss()
-                            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
-                            Log.d("ShopFragment", "onError")
-                        }
+                                    override fun onError(e: Throwable) {
+                                        processDialog.dismiss()
+                                        Toast.makeText(context, e.message, Toast.LENGTH_SHORT)
+                                            .show()
+                                    }
 
-                        override fun onComplete() {
-                            processDialog.dismiss()
-                            Log.d("ShopFragment", "onComplete")
+                                    override fun onComplete() {
+                                        processDialog.dismiss()
+                                    }
+                                })
                         }
                     })
+
             }
         }
     }
